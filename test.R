9bc7ead7c9e2e57c86a2b281cdd9db43bc57257a
godongyoung <dygo@naver.com>
2019-09-08 03:40:07 +0000

    commit test to new repo

---
+++ test.R
@@ -0,0 +1,7 @@
+print('this is test for R aws& githup')
+
+#https://www.r-bloggers.com/rstudio-and-github/
+# git remote add origin https://https://github.com/godongyoung/R_git_test.git
+# git config remote.origin.url git@github.com:godongyoung/R_git_test.git
+# git pull -u origin master
+# git push -u origin master
