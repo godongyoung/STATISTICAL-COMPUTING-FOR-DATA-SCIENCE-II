e14ad97c22fb5e472900ea15e639688888e6d4c1
godongyoung <dygo@naver.com>
2019-09-08 03:44:50 +0000

    commit test

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 print('this is test for R aws& githup')
 
 #https://www.r-bloggers.com/rstudio-and-github/
-# git remote add origin https://https://github.com/godongyoung/R_git_test.git
-# git config remote.origin.url git@github.com:godongyoung/R_git_test.git
+# git remote add origin https://https://github.com/godongyoung/STATISTICAL-COMPUTING-FOR-DATA-SCIENCE-II.git
+# git config remote.origin.url git@github.com:godongyoung/STATISTICAL-COMPUTING-FOR-DATA-SCIENCE-II.git
 # git pull -u origin master
 # git push -u origin master
